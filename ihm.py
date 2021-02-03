9e88e71ec1e3167373ae32b07f6a72db2c829a87
julie <julie.courgibet@cgi.com>
2021-02-03 11:29:10 +0100

    code non fonctionnel PyQt

--- ihm.py
+++ ihm.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication
 import tkinter as tk
-from qt_classes.fenetres import FenetrePrincipale
+from qt_classes.fenetres import Fenetre
 
 
 app = QApplication.instance() 
@@ -8,7 +8,7 @@ if not app: # sinon on crée une instance de QApplication
     app = QApplication(["Moodle"])
 
 # création d'une fenêtre avec QWidget dont on place la référence dans fen
-fen = FenetrePrincipale()
+fen = Fenetre()
 fen.show()
 
 # exécution de l'application, l'exécution permet de gérer les événements
--- qt_classes/fenetres.py
+++ qt_classes/fenetres.py
@@ -1,20 +1,14 @@
 import tkinter as tk
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QAction, qApp
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QGridLayout, QPushButton, QAction, qApp, QLabel
 from PyQt5.QtCore import Qt
 
 
-class FenetrePrincipale(QMainWindow):
+class FenetreAccueil(QMainWindow):
 	def __init__(self):
 		super().__init__()
 		self.setParametersUI()
 		self.setUI()
-
-	def setParametersUI(self):
-		root = tk.Tk()
-		screen_width = root.winfo_screenwidth()
-		screen_height = root.winfo_screenheight()
-		self.setGeometry(int(0.07*screen_width), int(0.07*screen_height), int(0.7*screen_width), int(0.7*screen_height))
-		self.setWindowTitle('Fenêtre principale')
+		
 
 	def setUI(self):
 		menu = self.menuBar()
@@ -24,20 +18,33 @@ class FenetrePrincipale(QMainWindow):
 		self.barreOutils = self.addToolBar('Quitter')
 		self.barreOutils.addAction(exitAction)       
 		self.statusBar().showMessage('Barre de statut')
+	
+	def activate(self):
+		self.fenetre.show()
 
 
 
 class Fenetre(QWidget):
 	def __init__(self):
 		super().__init__()
+		# Paramètres
 		self.parameters()
+		# Fenêtre initiale accueil
+		self.set_layout_fenetre_initiale()
+
 		# Elements
-		self.bouton1 = QPushButton("test 1")
-		self.bouton1.clicked.connect(self.click_1)
-		self.bouton2 = QPushButton("test 2")
-		self.bouton2.clicked.connect(self.click_2)
-		# Laytout
-		self.layout()
+		# self.bouton1 = QPushButton("test 1")
+		# self.bouton1.clicked.connect(self.click_1)
+		# self.bouton2 = QPushButton("test 2")
+		# self.bouton2.clicked.connect(self.click_2)
+		# self.bouton3 = QPushButton("test 3")
+		# self.bouton3.clicked.connect(self.click_1)
+		# self.bouton4 = QPushButton("test 4")
+		# self.bouton4.clicked.connect(self.click_2)
+		# self.bouton1.setFixedWidth(100)
+		# self.bouton2.setFixedWidth(100)
+		# self.bouton3.setFixedWidth(100)
+		# self.bouton4.setFixedWidth(100)
 
 	def parameters(self):
 		self.setWindowTitle("Outil de suivi des élèves")
@@ -51,11 +58,70 @@ class Fenetre(QWidget):
 		# Options
 		self.setMouseTracking(False)
 	
-	def layout(self):
-		self.layout = QVBoxLayout()
-		self.layout.addWidget(self.bouton1)
-		self.layout.addWidget(self.bouton2)
-		self.setLayout(self.layout)
+	def set_layout_fenetre_initiale(self):
+		# Main layout
+		self.layout = QGridLayout()
+		self.layout.fillWidth = False
+		self.layout.fillHeight = False
+		# Background color
+		self.setAutoFillBackground(True)
+		p = self.palette()
+		p.setColor(self.backgroundRole(), Qt.gray)
+		self.setPalette(p)
+		# Séparation de la fenêtre en 3 zones
+		# Zone titre
+		self.layout_titre = QGridLayout()
+		self.layout_titre.fillWidth = False
+		self.layout_titre.fillHeight = False
+		# self.layout_titre.setStyleSheet("background-color: gray;")
+		# Zone prof
+		self.layout_prof = QGridLayout()
+		self.layout_prof.fillWidth = False
+		self.layout_prof.fillHeight = False
+		# self.layout_prof.setStyleSheet("background-color: green;")
+		# Zone eleve
+		self.layout_eleve = QGridLayout()
+		self.layout_eleve.fillHeight = False
+		self.layout_eleve.fillWidth = False
+		# self.layout_eleve.setStyleSheet("background-color: green;")
+		# Ajout des sous-layout au principal
+		self.layout.addItem(self.layout_titre, 0, 0, 1, 2)
+		self.layout.addItem(self.layout_prof, 1, 0)
+		self.layout.addItem(self.layout_eleve, 1, 1)
+		# Laytout titre
+		self.layout_titre_fprincipale()
+		self.layout_prof_fprincipale()
+		self.layout_eleve_fprincipale()
+		# Layout1
+		# self.layout1.addWidget(self.bouton1, 0, 0)
+		# self.layout1.addWidget(self.bouton2, 1, 0)
+		# Layout1
+		# self.layout2.addWidget(self.bouton3, 0, 0)
+		# self.layout2.addWidget(self.bouton4, 0, 1)
+		# self.setLayout(self.layout)
+
+	def layout_titre_fprincipale(self):
+		self.titre_main = QLabel(self)
+		self.titre_main.setText("<font color=black>Outil de gestion Moodle</font>")
+		self.layout_titre.addWidget(self.titre_main, 0, 0)
+
+	def layout_prof_fprincipale(self):
+		self.titre_prof = QLabel(self)
+		self.titre_prof.setText("<font color=white>Professeur</font>")
+		self.layout_prof.addWidget(self.titre_prof, 0, 0)
+		self.bouton1 = QPushButton("test 1")
+		self.bouton1.clicked.connect(self.click_1)
+		self.bouton1.setFixedWidth(100)
+		self.layout_prof.addWidget(self.bouton1, 1, 0)
+
+	def layout_eleve_fprincipale(self):
+		self.titre_eleve = QLabel(self)
+		self.titre_eleve.setText("<font color=white>Elève</font>")
+		self.layout_eleve.addWidget(self.titre_eleve, 0, 0)
+		self.bouton2 = QPushButton("test 1")
+		self.bouton2.clicked.connect(self.click_2)
+		self.bouton2.setFixedWidth(100)
+		self.layout_eleve.addWidget(self.bouton1, 1, 0)
 
 	def mousePressEvent(self, event):
 		if event.button() == Qt.LeftButton:
@@ -75,6 +141,7 @@ class Fenetre(QWidget):
 	
 	def click_1(self):
 		print("appui 1")
+		self.bouton2.hide()
 
 	def click_2(self):
 		print("appui 2")
